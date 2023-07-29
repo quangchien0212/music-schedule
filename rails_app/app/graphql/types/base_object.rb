da1960d45c11b0ea905487d0355faeff18d3a331
quangchien0212 <quangchien0212@gmail.com>
2023-07-29 20:56:26 +0700

    use kaminari paginate for graphql

--- rails_app/app/graphql/types/base_object.rb
+++ rails_app/app/graphql/types/base_object.rb
@@ -1,5 +1,7 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
+    extend Paginations::KaminariPagination
+
     edge_type_class(Types::BaseEdge)
     connection_type_class(Types::BaseConnection)
     field_class Types::BaseField
