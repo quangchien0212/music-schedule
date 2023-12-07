6c3ba5cc1bdac9cd8130c2d678736184a7a673d4
quangchien0212 <quangchien0212@gmail.com>
2023-12-07 15:37:45 +0700

    chore: course model validates

--- rails_app/app/models/course.rb
+++ rails_app/app/models/course.rb
@@ -1,9 +1,39 @@
 class Course < ApplicationRecord 
+  STATUSES = %w(
+    active
+    disabled
+    deleted
+  )
+
+  LEVELS = %w(
+    beginner
+  )
+
   validates :name, presence: true
-  validates :status, inclusion: { in: %w(active) }
-  validates :level, inclusion: { in: %w(beginner) }
+  validates :status, inclusion: { in: STATUSES }
+  validates :level, inclusion: { in: LEVELS }
+  validates :price, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :lessons_to_complete, :numericality => { :greater_than_or_equal_to => 0 }
+
+  private
+
+  def set_as_deleted!
+    update!(status: 'deleted')
+  end
+
+  def active?
+    status == 'active'
+  end
+
+  def disabled?
+    status == 'disabled'
+  end
+
+  def deleted?
+    status == 'deleted'
+  end
 
   def deletable?
-    true
+    !active?
   end
 end
